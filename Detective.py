65867b58bd79eff478933af446ba92aba7767950
Ruangsak Patomwong <6130466521@student.chula.ac.th>
2021-04-03 17:29:59 +0700

    Update Detective.py

--- Detective.py
+++ Detective.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 def Detect(Address):
-  Address = str(input())
+ 
   URL = 'https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=' + Address + '&startblock=0&endblock=999999999&sort=asc&apikey=YourApiKeyToken'
   df = pd.read_json(URL)
   _df = []
@@ -12,3 +12,6 @@ def Detect(Address):
 def transact(df):
   for data in df:
     print('Hash:'+data['blockHash'] +' '+'From:'+data['from']+' '+'To:'+data['to']+' '+'Amount'+str(int(data['value'])/10**18))
+Address = str(input())
+data = Detect(Address)
+transact(data)
