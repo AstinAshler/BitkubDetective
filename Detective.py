751631e2542545a0da025e0ff73cbdd12a8fcc60
Ruangsak Patomwong <6130466521@student.chula.ac.th>
2021-04-03 17:26:54 +0700

    Update Detective.py

--- Detective.py
+++ Detective.py
@@ -1,10 +1,14 @@
 import numpy as np
 import pandas as pd
-Address = str(input())
-URL = 'https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=' + Address + '&startblock=0&endblock=999999999&sort=asc&apikey=YourApiKeyToken'
-df = pd.read_json(URL)
-_df = []
-for i in x:
-  if i['tokenSymbol'] == 'BKTC':
-   _df.append(i)
-_df
+def Detect(Address):
+  Address = str(input())
+  URL = 'https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=' + Address + '&startblock=0&endblock=999999999&sort=asc&apikey=YourApiKeyToken'
+  df = pd.read_json(URL)
+  _df = []
+  for i in x:
+    if i['tokenSymbol'] == 'BKTC':
+      _df.append(i)
+  return _df
+def transact(df):
+  for data in df:
+    print('Hash:'+data['blockHash'] +' '+'From:'+data['from']+' '+'To:'+data['to']+' '+'Amount'+str(int(data['value'])/10**18))
